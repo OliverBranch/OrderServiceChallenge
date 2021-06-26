079763d49d5192cce5788ae62a143846390fabd2
Israel Oliveira <israeloliveira001@gmail.com>
2021-06-26 13:49:34 -0300

    CRUD Scaffolding

---
+++ OrderServiceChallenge/Data/OrderServiceChallengeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderServiceChallenge.Models;
+
+namespace OrderServiceChallenge.Data
+{
+    public class OrderServiceChallengeContext : DbContext
+    {
+        public OrderServiceChallengeContext (DbContextOptions<OrderServiceChallengeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OrderServiceChallenge.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ OrderServiceChallenge/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderServiceChallenge.Models
+{
+    public class Company: LegalPerson
+    {
+    }
+}
