a146ede7d3687bb27c7b819860d03519b2ff91de
Israel Oliveira <israeloliveira001@gmail.com>
2021-06-26 13:38:41 -0300

    Project Created and some Entites

---
+++ OrderServiceChallenge/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderServiceChallenge.Models
+{
+    public class Employee : Person
+    {
+        public string CPF { get; set; }
+    }
+}
---
+++ OrderServiceChallenge/Models/LegalPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderServiceChallenge.Models
+{
+    public abstract class LegalPerson: Person
+    {
+        //[Display(Name = "CNPJ Company")]
+        [DisplayFormat(DataFormatString = "{0:000\\.000\\.000-00}", ApplyFormatInEditMode = true)]
+        [Range(10000000000000, 99999999999999, ErrorMessage = "Digite um valor válido de CNPJ (14 digitos)")]
+        public string CNPJ { get; set; }
+        [Range(10000000, 99999999, ErrorMessage = "Digite um valor válido de CEP (8 digitos)")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:#####-###}")]
+        public string CEP { get; set; }
+    }
+}
